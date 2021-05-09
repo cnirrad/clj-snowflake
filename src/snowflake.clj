c4b410029905d9bd993fb9e884f058445cd1dba9
Darrin Collins <cnirrad@gmail.com>
2021-05-09 14:44:09 -0500

    Created parse-id function to pick an ID back apart.

--- src/snowflake.clj
+++ src/snowflake.clj
@@ -1,28 +1,46 @@
 (ns snowflake)
 
+(def num-sequence-number-bits 12)
 (def max-sequence-number 4095)                              ; 2^12 - 1
+
+(def num-node-id-bits 10)
 (def max-node-id 1023)                                      ; 2^10 - 1
 
+(def default-epoch 1577836800000)                           ; 2020-01-01T00:00:00Z
+
+(defn create-id [ts node-id sequence]
+  (let [ts-bits (bit-shift-left ts (+ num-sequence-number-bits num-node-id-bits))
+        node-bits (bit-shift-left node-id num-sequence-number-bits)]
+    (bit-or ts-bits node-bits sequence)))
 
 (defn updt-context [context get-time]
   (let [ts (get-time)
         curr-seq (:sequence context)
         new-seq (if (= ts (:ts context))
                   (let [new-seq (mod (inc curr-seq) max-sequence-number)]
-                    (when (= new-seq 0)
-                      (Thread/sleep 1))
+                    (when (< new-seq curr-seq)              ; sequence rolled over, need to wait for the next ms
+                        (Thread/sleep 1))
                     new-seq)
                   0)]
     (assoc context :ts (get-time) :sequence new-seq)))
 
 (defn create-snowflake-generator [{:keys [epoch node-id get-time]
-                                   :or   {epoch    1451606400000
+                                   :or   {epoch    default-epoch
                                           node-id  1
                                           get-time (fn [] (System/currentTimeMillis))}}]
-  ; TODO validate inputs
+  (if (> node-id max-node-id)
+    (throw (IllegalArgumentException. (str "node-id cannot be greater than " max-node-id))))
   (let [context (atom {:ts 0 :sequence 0})]
     (fn []
       (let [{:keys [ts sequence]} (swap! context updt-context get-time)
-            ts-bits (bit-shift-left (- ts epoch) 22)
-            node-bits (bit-shift-left node-id 12)]
-        (bit-or ts-bits node-bits sequence)))))
+            ts (- ts epoch)]
+        (create-id ts node-id sequence)))))
+
+
+(defn parse-id [id]
+  (let [sequence (bit-and id 0xFFF)
+        node-id (bit-shift-right (bit-and id 0x3FF000) num-sequence-number-bits)
+        ts (bit-shift-right (bit-and id 0x7FFFFFFFFFC00000) (+ num-sequence-number-bits num-node-id-bits))]
+    {:sequence sequence
+     :node-id node-id
+     :ts ts}))
--- test/snowflake_test.clj
+++ test/snowflake_test.clj
@@ -1,8 +1,16 @@
 (ns snowflake-test
   (:require [clojure.test :refer :all]
-            [snowflake :refer [create-snowflake-generator]]))
+            [snowflake :refer [create-id create-snowflake-generator parse-id]]))
 
-(deftest generate-unique-id-test
+(deftest generate-snowflakes
   (let [id-gen (create-snowflake-generator {})]
     (testing "ID's should always be incrementing"
-      (is (< (id-gen) (id-gen) (id-gen))))))
+      (is (< (id-gen) (id-gen) (id-gen))))
+
+
+    (testing "Generation and parsing Id's should round trip."
+      (is (= {:ts 123456789 :node-id 777 :sequence 1234}
+            (parse-id (create-id 123456789 777 1234)))))
+
+    (testing "ID's should always be unique"
+      (is (= 50000 (count (into #{} (for [n (range 0 50000)] (id-gen)))))))))
